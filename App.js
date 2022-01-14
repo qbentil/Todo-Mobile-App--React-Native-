060aa16674be58dc7c531d68bf01e49d0258c254
qbentil <Bentilshadrack72@gmail.com>
2022-01-14 23:23:13 +0000

    Updated UI

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
-import {Button, Platform, ScrollView, StyleSheet, Switch, Text, TextInput, View} 
+import {Button, Platform, ScrollView, StyleSheet, Switch, Text, TextInput, TouchableOpacity, View} 
 from 'react-native'
 
-import {Constants} from 'expo'
+import {Constants,} from 'expo-constants'
 import React from 'react'
+import { StatusBar } from 'expo-status-bar';
 
 let id = 0;
 
 const Todo = props => (
-  <View style = {styles.TodoContainer}>
-    <Switch value = {props.todo.checked} onValueChange = {props.onToggle}/>
-    <Button  title = "Delete" onPress = { props.onDelete} />
-    <Text>{props.todo.text}</Text>
-  </View>
+    <View style = {styles.item}>
+        <View style = {styles.left}>
+          <Switch value = {props.todo.checked} onValueChange = {props.onToggle}/>
+          <Text style={styles.text}>{props.todo.text}</Text>
+        </View>
+        <View>
+            <Button  
+              title = "Delete" 
+              onPress = { props.onDelete} 
+              style = {styles.circular}
+            />
+        </View>
+    </View>
 )
 
 export default class App extends React.Component{
@@ -68,8 +77,14 @@ export default class App extends React.Component{
       return (
         <View style = {[styles.fill, styles.AppContainer]}>
           <View style = {styles.HeaderContainer}>
-            <Text>TODO Count: {this.state.todos.length}</Text>
-            <Text>Unchecked TODO Count: {this.state.todos.filter(todo => !todo.checked).length}</Text>
+            <View style={styles.counters}>
+               <Text>TODO Count:</Text>
+              <Text style = {styles.count}>{this.state.todos.length}</Text>
+            </View>
+            <View style={styles.counters}>
+               <Text>Unchecked TODO Count:</Text>
+              <Text style = {styles.count}>{this.state.todos.filter(todo => !todo.checked).length}</Text>
+            </View>
           </View>
           <View style = {[styles.inputContainer]}>
             <TextInput
@@ -101,19 +116,18 @@ export default class App extends React.Component{
 }
 
 const styles = StyleSheet.create({
-  TodoContainer: {
-    flexDirection: 'row', 
-    alignItems: 'center',
-    justifyContent: "space-around",
-    paddingVertical: 10
-  },
   AppContainer: {
-    paddingTop: Constants.statusBarHeight
+    paddingTop: 45,
+    backgroundColor: "#e8eaed",
   },
   HeaderContainer: {
     flexDirection: 'row', 
     justifyContent: "space-around",
-    paddingVertical: 10
+    paddingVertical: 15,
+    paddingHorizontal:10,
+    margin: 20,
+    borderRadius: 20,
+    backgroundColor: "#fff"
   },
   fill: {
     flex: 1
@@ -121,17 +135,55 @@ const styles = StyleSheet.create({
   inputContainer: {
     flexDirection: 'row', 
     justifyContent: "space-around",
+    alignItems: "center",
     paddingVertical: 10
   },
+  counters: {
+    flexDirection: "row",
+    alignItems: "center"
+  },
+  count: {
+    fontWeight: "bold",
+    marginHorizontal: 5,
+    color: "#55bcf6",
+
+  },
   input: {
-    height: 40,
-    width: 250,
+    height: 45,
+    width: 300,
     borderWidth: 0.5,
     padding: 10,
+    borderRadius: 50,
   },
   button: {
     height: 40,
     padding: 10,
-  }
+  },
+  item: {
+    backgroundColor: '#fff',
+    padding: 5,
+    borderRadius: 20,
+    flexDirection: 'row',
+    alignItems: "center",
+    justifyContent: "space-between",
+    margin: 5,
+    marginBottom: 10,
+  },
+  left: {
+    flexDirection: 'row',
+    alignItems: "center",
+    flexWrap: "wrap"
+  },
+  circular:{
+    height: 40,
+    padding: 10,
+    borderColor: '#55bcf6',
+    borderWidth: 2,
+    borderRadius: 5 
+  },
+  text: {
+    maxWidth: "80%",
+    marginLeft: 10
+  },
 })
 
